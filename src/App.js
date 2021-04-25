56789f4d526ddd0e96420a828520c2aa71f24c30
yogeshu <yogeshbhavsar1994@gmail.com>
2021-04-25 16:15:06 +0530

    added function for pagination show 4 item per page

--- src/App.js
+++ src/App.js
@@ -1,53 +1,60 @@
-import React,{useState,useEffect} from 'react'
-import Checkbox from './components/Checkbox'
-import Search from './components/Search'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+
+import Checkbox from "./components/Checkbox";
+import Search from "./components/Search";
+import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
   const [loading, setLoding] = useState(false);
-
-   
-
-
+  const [currentPage, setCurrentPage] = useState(0);
+// fetching of data from json file 
   const getData = () => {
     fetch("./airports.json", {
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
-      }
+        Accept: "application/json",
+      },
     })
       .then(function (response) {
         setLoding(true);
         return response.json();
       })
       .then((data) => setData(data), setLoding(false));
-       console.log(data);
-     
+    //  console.log(data);
   };
-useEffect(()=>{
-
-getData();
-},[])
-
-
-
-
-
-
-
-
-
+// using useEffect for updating data 
+  useEffect(() => {
+    getData();
+  }, []);
+//  fucntion for pagination
+  const PER_PAGE = 4;
+  const offset = currentPage * PER_PAGE;
+  const currentPageData = data.slice(offset, offset + PER_PAGE);
+  const pageCount = Math.ceil(data.length / PER_PAGE);
  
 
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrentPage(selectedPage);
+  }
+
+  const paginationOffset = currentPage * PER_PAGE;
+
+  const paginatedEntries = data.slice(
+    paginationOffset,
+    paginationOffset + PER_PAGE
+  );
 
 
-  return ( 
-    
+
+
+
+  return (
     <div className="responsive-table">
-     <Checkbox/>
-     <Search/>
-      
+      <Checkbox />
+      <Search />
+
       {/* <label>
         small
         <input type="checkbox"
@@ -61,9 +68,8 @@ getData();
         large
         <input type="checkbox" />
       </label> */}
-     
+
       <table>
-      
         <thead>
           <tr>
             <th> Name </th>
@@ -76,9 +82,9 @@ getData();
           </tr>
         </thead>
         <tbody>
-        {data.length === 0 ? "Nodata" : null}
-          {data.length > 0 &&
-            data.map((d) => (
+          {currentPageData.length === 0 ? "nodata" : null}
+          {currentPageData.length > 0 &&
+            currentPageData.map((d) => (
               <tr key={d.id}>
                 <td> {d.name} </td>
                 <td> {d.icao} </td>
@@ -91,9 +97,6 @@ getData();
             ))}
         </tbody>
       </table>
-   
-
-
     </div>
   );
 }
