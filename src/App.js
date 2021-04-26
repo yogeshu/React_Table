e5898f98fd997fbaa7714aee56d60bcfdc5ab181
yogeshu <yogeshbhavsar1994@gmail.com>
2021-04-26 17:45:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Checkbox from "./components/Checkbox";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Checkbox from "./components/Checkbox";
 import Search from "./components/Search";
 import "./App.css";
-
-
+import {
+  FormControl,
+  
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+  TextField
+} from "@material-ui/core";
 function App() {
   const [data, setData] = useState([]);
   const [loading, setLoding] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
-  
-  const [search, setSearch] = useState("");
-  const [first,setFirst ]  = useState(true)
-  const [checkfilter, setCheckFilter] = useState({});
-  const [check ,setCheked] = useState(true)
 
+  const [search, setSearch] = useState("");
+  const [first, setFirst] = useState(true);
+  // const [checkfilter, setCheckFilter] = useState({});
+  const [check, setCheked] = useState(true);
+  const [filterCheck, setFilterCheck] = useState([]);
+  const [type, setType] = useState([]);
   
-  // using filter data on top for avoid rendring issue due to null varible 
+  // const [checkfilter, setCheckFilter] = useState(new Map());
+  // const [check ,setCheked] = useState(false)
+  //  const Arraryfilter = {
+
+  //    filters: [
+  //    "small", "large", "medium" , "fake"
+  //    ]
+  //  }
+  const typeData = [
+    { type: "small" },
+    { type: "medium" },
+    { type: "large" },
+    { type: "closed" },
+    { type: "heliport" }
+    // { type: "in your favorites" }
+  ];
+
+  const handleChange = (e) => {
+    if (e.target.checked) {
+     
+      setType([...type, e.target.value]);
+    } else {
+      setType(type.filter((id) => id !== e.target.value));
+    }
+    console.log(type)
+
+  };
+
+  useEffect(() => {
+    if (type.length === 0) {
+      setFilterCheck(data);
+    } else {
+      setFilterCheck(
+        data.filter((typeData) =>
+          type.some((category) => [typeData.type].flat().includes(category))
+        )
+      );
+    }
+  }, [type,data]);
+
+  // using filter data on top for avoid rendring issue due to null varible
   const filterData = data.filter((types) => {
     return (
-     
-      
-    
       types.name
         .toString()
         .toLowerCase()
@@ -39,17 +83,17 @@ function App() {
       types.longitude
         .toString()
         .toLowerCase()
-        .indexOf(search.toLocaleLowerCase()) !== -1||
-        types.type
+        .indexOf(search.toLocaleLowerCase()) !== -1 ||
+      types.type
         .toString()
         .toLowerCase()
         .indexOf(search.toLocaleLowerCase()) !== -1
     );
   });
+  
+  // fitelr data
 
-  // fitelr data 
-
-  // fetching of data from json file 
+  // fetching of data from json file
   const getData = () => {
     fetch("./airports.json", {
       headers: {
@@ -64,28 +108,25 @@ function App() {
       .then((data) => setData(data), setLoding(false));
     //  console.log(data);
   };
-// using useEffect for updating data 
+  // using useEffect for updating data
   useEffect(() => {
     getData();
-  }, [search]);
+  }, [search,filterCheck]);
 
-
-
-
-//  fucntion for pagination
+  //  fucntion for pagination
   const PER_PAGE = 4;
   const offset = currentPage * PER_PAGE;
-  const currentPageData = filterData.slice(offset, offset + PER_PAGE);
+  const currentPageData = filterData.slice(offset, offset + PER_PAGE) && filterCheck.slice(offset, offset + PER_PAGE);
   const pageCount = Math.ceil(filterData.length / PER_PAGE);
   const handleSearchName = (e) => {
     setSearch(e.target.value);
   };
 
   function handleCheck(e) {
-    setFirst(!first)
-    console.log(first)
+    setFirst(!first);
+    console.log(first);
   }
-  
+
   function handlePageClick({ selected: selectedPage }) {
     setCurrentPage(selectedPage);
   }
@@ -95,42 +136,65 @@ function App() {
   const paginatedEntries = filterData.slice(
     paginationOffset,
     paginationOffset + PER_PAGE
+  ) || filterCheck.slice(
+    paginationOffset,
+    paginationOffset + PER_PAGE
   );
- 
 
-  
-  const checkData = ["small", "large","medium", "heliport","closed", "at your favorite"];
-  const handleInputCheck = (e) => {
-   
-      setCheckFilter({ ...checkfilter, [e.target.name]: e.target.checked });
-     console.log(checkfilter)
-  
-    // const newdata = data.filter((types) => {
-    //   return types.type.includes(e.target.name);
-  
-    // });
-    if (e.target.checked) {
-      setData(
-        data.filter((types) => {
-          return types.type.includes(e.target.name);
-        })
-      );
-    } else {
-      setData(data);
-    }
+  // const checkData = [
+  //   "small",
+  //   "large",
+  //   "medium",
+  //   "heliport",
+  //   "closed",
+  //   "at your favorite",
+  // ];
+  // const handleInputCheck = (e) => {
+  //   // setCheckFilter({ ...checkfilter, [e.target.name]: e.target.checked });
+  //   // console.log(checkfilter);
 
-    console.log(checkfilter);
-  };
+  //   // const newdata = data.filter((types) => {
+  //   //   return types.type.includes(e.target.name);
 
+  //   // });
+  //   if (e.target.checked) {
+  //     setData(
+  //       data.filter((types) => {
+  //         return types.type.includes(e.target.name);
+  //       })
+  //     );
+  //   } else {
+  //     setData(data);
+  //   }
 
+  //   // console.log(checkfilter);
+  // };
 
   return (
     <div className="responsive-table">
-      <h1> Filter <span className="black"> Airports </span></h1>
-       <div className="fake-list">
-      <Checkbox checkData={checkData} handleInputCheck={handleInputCheck} checkfilter={checkfilter} check={check} />
-      <Search search={search} handleSearchName={handleSearchName} data={data}  />
+      <h1>
+        {" "}
+        Filter <span className="black"> Airports </span>
+      </h1>
+      <div className="fake-list">
+      <div className="ucheckbox-list">
+      <span> Type </span> <br/>
+      {typeData.map((typeData) => (
+            <FormControlLabel
+              control={<Checkbox onChange={handleChange} />}
+              label={typeData.type} 
+              value={typeData.type}
+            />
+          ))}
+         </div> 
+
+        <Search
+          search={search}
+          handleSearchName={handleSearchName}
+          data={data}
+        />
       </div>
+      
       {/* <label>
         small
         <input type="checkbox"
@@ -144,6 +208,7 @@ function App() {
         large
         <input type="checkbox" />
       </label> */}
+      
 
       <table>
         <thead>
@@ -173,23 +238,27 @@ function App() {
             ))}
         </tbody>
       </table>
-    
-    <p  className="pagination__link"> Showing ({paginationOffset + 1}-
-        {paginationOffset + paginatedEntries.length} of {filterData.length}) Results </p>
-        <div className="page">
-      <ReactPaginate
-        previousLabel={"← "}
-        nextLabel={"→"}
-        // pageCount={pageCount}
-        onPageChange={handlePageClick}
-        // pageRangeDisplayed={2}
-        pageLinkClassName={"pageCount"}
-        containerClassName={"pagination"}
-        previousLinkClassName={"pagination__link"}
-        nextLinkClassName={"pagination__link"}
-        disabledClassName={"pagination__link--disabled"}
-        activeClassName={"pagination__link--active"}
-      />
+
+      <p className="pagination__link">
+        {" "}
+        Showing ({paginationOffset + 1}-
+        {paginationOffset + paginatedEntries.length} of {filterData.length })
+        Results{" "}
+      </p>
+      <div className="page">
+        <ReactPaginate
+          previousLabel={"← "}
+          nextLabel={"→"}
+          // pageCount={pageCount}
+          onPageChange={handlePageClick}
+          // pageRangeDisplayed={2}
+          pageLinkClassName={"pageCount"}
+          containerClassName={"pagination"}
+          previousLinkClassName={"pagination__link"}
+          nextLinkClassName={"pagination__link"}
+          disabledClassName={"pagination__link--disabled"}
+          activeClassName={"pagination__link--active"}
+        />
       </div>
     </div>
   );
